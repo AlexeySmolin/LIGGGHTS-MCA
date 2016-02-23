3d4d297d41a3ec17bd08e5ea2047228427449100
Nadia Salman [RPG] <mnnsa@men-pc4032.leeds.ac.uk>
2016-02-23 12:38:55 +0000

    Changes needed from sphere

--- src/atom_vec_mca.h
+++ src/atom_vec_mca.h
@@ -34,6 +34,7 @@
     Contributing author and copyright for this file:
 
     Alexey Smolin (ISPMS SB RAS, Tomsk, Russia, http://www.ispms.ru)
+    Nadia Salman (iT-CDT, Leeds, UK)
 
     Copyright 2016-     ISPMS SB RAS, Tomsk, Russia
 ------------------------------------------------------------------------- */
@@ -48,6 +49,7 @@ AtomStyle(mca,AtomVecMCA)
 #define LMP_ATOM_VEC_MCA_H
 
 #include "atom_vec.h"
+#include "fix_bond_exchange_mca.h"
 
 namespace LAMMPS_NS {
 
@@ -55,19 +57,25 @@ class AtomVecMCA : public AtomVec {
  public:
   AtomVecMCA(class LAMMPS *);
   void settings(int narg, char **arg);
-  ~AtomVecMCA(); //{}; fbe освобождать
+  ~AtomVecMCA(); //{}; fbe - delete?
   void init();
   void grow(int);
   void grow_reset();
   void copy(int, int, int);
   int pack_comm(int, int *, double *, int, int *);
   int pack_comm_vel(int, int *, double *, int, int *);
+  int pack_comm_vel_wedge(int, int *, double *, int, int *); ///AS ///////////
+  int pack_comm_hybrid(int, int *, double *); ////////////////
   void unpack_comm(int, int, double *);
   void unpack_comm_vel(int, int, double *);
+  int unpack_comm_hybrid(int , int , double *); ///AS
   int pack_reverse(int, int, double *);
+  int pack_reverse_hybrid(int, int, double *); //////////////////
   void unpack_reverse(int, int *, double *);
+  int unpack_reverse_hybrid(int, int *, double *);  ///////////////////
   int pack_border(int, int *, double *, int, int *);
   int pack_border_vel(int, int *, double *, int, int *);
+  int pack_border_vel_wedge(int, int *, double *, int, int *); ///AS ///////////
   int pack_border_hybrid(int, int *, double *);
   void unpack_border(int, int, double *);
   void unpack_border_vel(int, int, double *);
@@ -80,26 +88,37 @@ class AtomVecMCA : public AtomVec {
   void create_atom(int, double *);
   void data_atom(double *, int, char **);
   int data_atom_hybrid(int, char **);
+  void data_vel(int, char **);  /////////////
+  int data_vel_hybrid(int, char **);  /////////////
   bigint memory_usage();
   //new for L3
   void pack_data(double **);
   void pack_data(double **buf,int tag_offset); 
+  int pack_data_hybrid(int, double *); ////////////??
   void write_data(FILE *, int, double **);
+  int write_data_hybrid(FILE *, double *); /////////??
   void write_restart_settings(FILE *);
   void read_restart_settings(FILE *);
+  void pack_vel(double **); ///////////
+  void pack_vel(double **buf,int tag_offset); //////////
+  int pack_vel_hybrid(int, double *); ////////////??
+  void write_vel(FILE *, int, double **); /////////
+  int write_vel_hybrid(FILE *, double *); //////??
 
  private:
-   //!! Это есть в Sphere
+   //!! All these are in Sphere
   int *tag,*type,*mask; //atom ID, atom type?, deform_groupbit?
-  tagint *image;        // IMGMAX-IMG2BITS?? 
+  tagint *image;        // IMGMAX-IMG2BITS in new version of LIGGGHTS?? 
   double **x,**v,**f;
-  //!! Если добавить Моё, то не надо делать "atom_style hybrid"
-  /* {Моё 
-  //double *radius,*rmass; //НЕ НУЖНО: int mass_type;  // 1 if per-type masses
-  double **angmom; //Будем пока думать, что это момент инерции
-  double **theta,**omega,**torque; //theta - пока будем векторами описывать ориентацию
-  // В будущем нужно перейти на кватернионы, как в AtomVecEllipsoid struct Bonus {double quat[4];
-  // }Моё */
+  double *radius,*density,*rmass; //////////!!Later change as 'double radius,rmass;' and  int mass_type;  // 1 if per-type masses
+  double **omega,**torque; //////////
+  int radvary; ///////////
+
+  /* { //!! Later we will need these
+  double **angmom; // ?? We need moment of inertia
+  double **theta //theta - we need orientation vector to describe rotation as a first approximation
+  // Later we must use quaternions or Rodrigues rotation vector , as in AtomVecEllipsoid 'struct Bonus {double quat[4]...};'
+  // } */
 /*
 For the MCA style, the number of mca bonds per atom is stored, and the information associated to it:
  the type of each bond, the ID of the bonded atom and the so-called bond history.
@@ -110,16 +129,16 @@ In atom_style command it need 2 args: the number of bond types, and the maximum 
 ??? For each bond type, the parameters have to be specified via the bond_coeff command (see example here).
 
 An example for the sytnax is given below:
-atom_style mca n_bondtypes 1 bonds_per_atom 6  (6 - кубическая, 12 - гцк)
+atom_style mca n_bondtypes 1 bonds_per_atom 6  (6 - cubic packing, 12 - fcc packing)
 */
-  int *molecule; //!! Нужно ли?
+  int *molecule; //!! This allows to have bonds. Do we really need it?
   int **nspecial,**special;
   int *num_bond;
   int **bond_type,**bond_atom;
   int num_bondhist;
   double ***bond_hist;
 
-  class FixBondExchangeMCA *fbe; //!! Это для обмена между процессорами. Почему нет include?*
+  class FixBondExchangeMCA *fbe; //!! This is used for MPI eschange as I understand. But there is no '#include ...' Why?
 };
 
 }
