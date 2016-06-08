35f2490d0bd948ed58b17d83864d0da09516bb75
Nadia Salman [RPG] <mnnsa@men-pc4032.leeds.ac.uk>
2016-06-08 16:07:11 +0100

    unloading

--- src/version_liggghts.h
+++ src/version_liggghts.h
@@ -1,1 +1,1 @@
-#define LIGGGHTS_VERSION "LIGGGHTS-PUBLIC 3.3.1, compiled 2016-05-14-19:11:09 by asmolin, git commit bfcd28f805230f51fbc3136685fb6b6a2b847bcd"
+#define LIGGGHTS_VERSION "LIGGGHTS-PUBLIC 3.3.1, compiled 2016-06-08-12:17:04 by mnnsa, git commit baa729064a136ae17c71eed08994194735e48bfd"
